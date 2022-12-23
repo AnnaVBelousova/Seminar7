00549cfaf92a9b3e6a8d1c894dad0b264a3367ca
anna <a1b2v3@mail.ru>
2022-12-23 18:36:14 +0500

    add ex50

---
+++ Ex50/Program.cs
@@ -0,0 +1,71 @@
+﻿void PrintArray(int[,]matrix)
+{
+// int rows; 
+// int column;
+// int[,]matrix = new int [0, 1];
+for (int rows = 0; rows < matrix.GetLength(0); rows ++)
+{
+    for (int column = 0; column < matrix.GetLength(1); column ++)
+    {
+       Console.Write($" {matrix[rows, column]}");
+    }
+    Console.WriteLine();
+}
+}
+
+    void FillArray (int[,]matrix)
+{
+    for (int i  = 0; i< matrix.GetLength(0); i++)
+    {
+       for (int j  = 0; j< matrix.GetLength(1); j++)
+        {
+           matrix[i,j] = new Random().Next(1,10); 
+        }
+    }
+
+}
+
+
+int Valueposition(int i, int j, int[,]matrix)
+{
+    if (i> matrix.GetLength(0) || j > matrix.GetLength(1) ) 
+    {
+       Console.WriteLine ("Такого значения нет");
+    }
+     
+   else 
+    {
+        int value = matrix[i,j];
+    }
+   {
+   return matrix[i,j];
+   }
+    }
+    
+   
+
+  
+
+   
+     
+
+
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int row  = int.Parse (Console.ReadLine());
+int col  = int.Parse (Console.ReadLine());
+int[,]matr = new int [row, col];
+
+
+FillArray(matr);
+PrintArray(matr);
+Valueposition(4,1,matr);
+
+//Valueposition(1,1);
+Console.WriteLine(matr[4,1]);
+
+
+
+
+
